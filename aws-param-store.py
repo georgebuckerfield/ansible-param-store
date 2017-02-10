163dd10af6130434fbd9f7a35ea0fc73ce32b2d1
george-buckerfield <georgebuckerfield@gmail.com>
2017-02-10 20:12:26 +0000

    Make a connection to SSM, retrieve the parameter and decrypt it

--- aws-param-store.py
+++ aws-param-store.py
@@ -11,10 +11,28 @@ class LookupModule(LookupBase):
 
     def run(self, terms, variables, **kwargs):
 
-        ret = []
+        try:
+            import boto3
+        except ImportError:
+            raise AnsibleError('Encountered exception while importing boto3')
+        client = boto3.client('ssm')
 
+
+        ret = []
         for term in terms:
             try:
+                response = client.get_parameters(
+                    Names=[
+                        term,
+                    ],
+                    WithDecryption=True
+                )
+                if len(response['InvalidParameters']) > 0:
+                    raise AnsibleError('Invalid parameter')
+                else:
+                    val = response['Parameters'][0]['Value']
+            except Exception as e:
+                raise AnsibleError('Encountered exception
                 # Check SSM parameter store here
             except Exception as e:
                 raise AnsibleError('Encountered exception while fetching {0}: {1}'.format(term, e.message))
