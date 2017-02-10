a4b89b8d2ac4e2e9caa81bd77655e79b58e63a4d
george-buckerfield <georgebuckerfield@gmail.com>
2017-02-10 20:17:11 +0000

    Allow the user to specify if the paramater value should decrpyted when they call the plugin,using decrypt=True|False

--- aws-param-store.py
+++ aws-param-store.py
@@ -17,15 +17,22 @@ class LookupModule(LookupBase):
             raise AnsibleError('Encountered exception while importing boto3')
         client = boto3.client('ssm')
 
+        try:
+            decrypt = kwargs['decrypt']
+        except KeyError:
+            display.vvvv("No decryption option specified, defaulting to False")
+            decrypt = False
+
 
         ret = []
         for term in terms:
+            display.vvvv("Retrieving %s from EC2 Parameter Store" % term)
             try:
                 response = client.get_parameters(
                     Names=[
                         term,
                     ],
-                    WithDecryption=True
+                    WithDecryption=decrypt
                 )
                 if len(response['InvalidParameters']) > 0:
                     raise AnsibleError('Invalid parameter')
